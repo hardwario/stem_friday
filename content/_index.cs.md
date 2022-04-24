4814cf0315405951becc3ac916b474a1e2d1c400
Lukas Fabik <lukas.fabik@hardwario.com>
2022-04-24 15:49:43 +0200

    publish updates

--- content/_index.cs.md
+++ content/_index.cs.md
@@ -3,9 +3,9 @@ title: Úvod
 url: /
 menu: []
 weight: 1
-meta_title: STEM FRIDAY - Dostupný STEM program pro vaši školu
+meta_title: STEM Friday - Dostupný STEM program pro vaši školu
 meta_description: 
-heading: Dostupný<br/> STEM program <br/>pro vaši školu
-perex: Stačí jedna hodina každý pátek.
+heading: Výuka internetu věcí pro vaši školu
+perex: Ucelený program podpory učitelů pro výuku internetu věcí na základních a středních školách. Zahrnuje elektronickou stavebnici, výukové materiály, vzorové lekce a technickou podporu.
 form_id: 7aa60c1f-d7c4-49c0-90ad-d4cc9d4aa558
 ---
--- gulpfile.js
+++ gulpfile.js
@@ -1,126 +1,71 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-const autoprefixer = require('gulp-autoprefixer');
-const child = require('child_process');
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const hash = require("gulp-hash");
-const del = require("del");
+const child = require('child_process');
 const hugo = child.spawn('hugo', ['serve', '--bind=0.0.0.0']);
-var purify = require('gulp-purifycss');
-let cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
 gulp.task('hugo', () => {
-  const hugo = child.spawn('hugo', ['serve']);
+    const hugo = child.spawn('hugo', ['serve']);
+  
+    const hugoLogger = (buffer) => {
+      buffer.toString()
+        .split(/\n/)
+        .forEach((message) => gutil.log('Hugo: ' + message))
+    };
+  
+    hugo.stdout.on('data', hugoLogger);
+    hugo.stderr.on('data', hugoLogger);
+  });
 
-  const hugoLogger = (buffer) => {
-    buffer.toString()
-      .split(/\n/)
-      .forEach((message) => gutil.log('Hugo: ' + message))
-  };
-
-  hugo.stdout.on('data', hugoLogger);
-  hugo.stderr.on('data', hugoLogger);
-});
-
-
-// Compile SCSS files to CSS
-gulp.task("scss-main", function () {
-    //del(["static/_assets/css/www/**/*"])
+//compile scss into css
+gulp.task("scss-main", async function () {
     gulp.src("src/scss/main.scss")
-    //del(["static/_assets/css/*"])
-        .pipe(sass({outputStyle : "compressed"}))
-        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
-        .pipe(purify(['static/_assets/js/**/*.js', 'layouts/**/*.html']))
-        .pipe(cleanCSS({debug: true}, (details) => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+        .pipe(sass().on('error',sass.logError))
         .pipe(hash())
-        .pipe(gulp.dest("static/_assets/css"))
-        //Create a hash map
+        .pipe(gulp.dest('static/_assets/css'))
         .pipe(hash.manifest("hash.json"))
-        //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        .pipe(gulp.dest("data/css"));
 });
 
-gulp.task("scss-main-addon", function () {
-    //del(["static/_assets/css/www/**/*"])
-    gulp.src("src/scss/main-addon.scss")
-    //del(["static/_assets/css/*"])
-        .pipe(sass({outputStyle : "compressed"}))
-        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
-        .pipe(purify(['static/_assets/js/**/*.js', 'layouts/**/*.html']))
-        .pipe(cleanCSS({debug: true}, (details) => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+gulp.task("scss-main-addon", async function () {
+    gulp.src('src/scss/main-addon.scss')
+        .pipe(sass().on('error',sass.logError))
         .pipe(hash())
-        .pipe(gulp.dest("static/_assets/css"))
-        //Create a hash map
+        .pipe(gulp.dest('static/_assets/css'))
         .pipe(hash.manifest("hash.json"))
-        //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        .pipe(gulp.dest("data/css"));
 });
 
-gulp.task("scss-font", function () {
-    //del(["static/_assets/css/www/**/*"])
-    gulp.src("src/scss/components/font.scss")
-    //del(["static/_assets/css/*"])
-        .pipe(sass({outputStyle : "compressed"}))
-        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
-        .pipe(purify(['static/_assets/js/**/*.js', 'layouts/**/*.html']))
-        .pipe(cleanCSS({debug: true}, (details) => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+gulp.task("scss-font", async function () {
+    gulp.src('src/scss/components/font.scss')
+        .pipe(sass().on('error',sass.logError))
         .pipe(hash())
-        .pipe(gulp.dest("static/_assets/css"))
-        //Create a hash map
+        .pipe(gulp.dest('static/_assets/css'))
         .pipe(hash.manifest("hash.json"))
-        //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        .pipe(gulp.dest("data/css"));
 });
 
-gulp.task("scss-bg", function () {
-    //del(["static/_assets/css/www/**/*"])
-    gulp.src("src/scss/components/bg.scss")
-    //del(["static/_assets/css/*"])
-        .pipe(sass({outputStyle : "compressed"}))
-        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
-        .pipe(purify(['static/_assets/js/**/*.js', 'layouts/**/*.html']))
-        .pipe(cleanCSS({debug: true}, (details) => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+gulp.task("scss-bg", async function () {
+    gulp.src('src/scss/components/bg.scss')
+        .pipe(sass().on('error',sass.logError))
         .pipe(hash())
-        .pipe(gulp.dest("static/_assets/css"))
-        //Create a hash map
+        .pipe(gulp.dest('static/_assets/css'))
         .pipe(hash.manifest("hash.json"))
-        //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        .pipe(gulp.dest("data/css"));
 });
 
-gulp.task("scss-icon", function () {
-    //del(["static/_assets/css/www/**/*"])
-    gulp.src("src/scss/components/icon.scss")
-    //del(["static/_assets/css/*"])
-        .pipe(sass({outputStyle : "compressed"}))
-        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
-        .pipe(purify(['static/_assets/js/**/*.js', 'layouts/**/*.html']))
-        .pipe(cleanCSS({debug: true}, (details) => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+gulp.task("scss-icon", async function () {
+    gulp.src('src/scss/components/icon.scss')
+        .pipe(sass().on('error',sass.logError))
         .pipe(hash())
-        .pipe(gulp.dest("static/_assets/css"))
-        //Create a hash map
+        .pipe(gulp.dest('static/_assets/css'))
         .pipe(hash.manifest("hash.json"))
-        //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        .pipe(gulp.dest("data/css"));
 });
 
-// Hash javascript
-gulp.task("js", function () {
+gulp.task("js", async function () {
     //del(["static/_assets/js/www/**/*"])
     gulp.src(["src/js/**/*", "node_modules/bootstrap/dist/js/bootstrap.min.js"])
         .pipe(hash())
@@ -130,20 +75,9 @@ gulp.task("js", function () {
 });
 
 // Watch asset folder for changes
-gulp.task("watch", ["js","scss-main","scss-main-addon","scss-font","scss-bg","scss-icon"], function () {
+gulp.task("watch", gulp.series("js","scss-main","scss-main-addon","scss-font","scss-bg","scss-icon"), async function () {
     gulp.watch("src/scss/**/*", ["scss-main"])
     gulp.watch("src/js/**/*", ["js"])
 });
 
-
-gulp.task("prefix", () => {
-  gulp.src('./public/**/*.css')
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest(function (file) {
-      return file.base;
-    }))
-});
-
-gulp.task('start', ['hugo', 'watch']);
+gulp.task('start', gulp.series('hugo', 'watch'));
